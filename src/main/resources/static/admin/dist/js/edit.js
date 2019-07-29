5f0f5d1d867b1be39f79d6703c892009997b9c10
Jonny <342418262@qq.com>
2019-07-29 23:21:24 +0800

    :art: update代码块皮肤样式

--- src/main/resources/static/admin/dist/js/edit.js
+++ src/main/resources/static/admin/dist/js/edit.js
@@ -21,6 +21,41 @@ $(function () {
         imageFormats: ["jpg", "jpeg", "gif", "png", "bmp", "webp"], //图片上传格式
         imageUploadURL: "/admin/blogs/md/uploadfile",
         onload: function (obj) { //上传成功之后的回调
+
+        }
+    });
+
+    // 编辑器粘贴上传
+    document.getElementById("blog-editormd").addEventListener("paste", function (e) {
+        var clipboardData = e.clipboardData;
+        if (clipboardData) {
+            var items = clipboardData.items;
+            if (items && items.length > 0) {
+                for (var item of items) {
+                    if (item.type.startsWith("image/")) {
+                        var file = item.getAsFile();
+                        if (!file) {
+                            alert("请上传有效文件");
+                            return;
+                        }
+                        var formData = new FormData();
+                        formData.append('file', file);
+                        var xhr = new XMLHttpRequest();
+                        xhr.open("POST", "/admin/upload/file");
+                        xhr.onreadystatechange = function () {
+                            if (xhr.readyState == 4 && xhr.status == 200) {
+                                var json=JSON.parse(xhr.responseText);
+                                if (json.resultCode == 200) {
+                                    blogEditor.insertValue("![](" + json.data + ")");
+                                } else {
+                                    alert("上传失败");
+                                }
+                            }
+                        }
+                        xhr.send(formData);
+                    }
+                }
+            }
         }
     });
 
